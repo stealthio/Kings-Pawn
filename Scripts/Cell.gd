f286a54b68a1df98dd23341a24d4235c96d38f5e
Paul Töpfer <hackerman@spaghetticoding.de>
2022-01-10 00:29:16 +0100

    Finalized Team Setup

--- Scripts/Cell.gd
+++ Scripts/Cell.gd
@@ -5,12 +5,24 @@ export var editable = true
 export var number = 0
 var tr
 var teamsetup
+var name_label
 
 func _ready():
 	teamsetup = get_tree().root.get_node("TeamSetup")
 	_create_figurine(figure)
 	if !editable:
 		modulate = Color.darkgray
+	name_label = get_parent().get_parent().get_node("PanelContainer/Name")
+	connect("mouse_entered", self, "on_mouse_entered")
+	connect("mouse_exited", self, "on_mouse_exited")
+
+func on_mouse_entered():
+	self_modulate = Color.red
+	name_label.text = figure
+
+func on_mouse_exited():
+	self_modulate = Color.white
+	name_label.text = ""
 
 func _on_Available_pressed():
 	if editable:
--- Scripts/PurchaseFigureButton.gd
+++ Scripts/PurchaseFigureButton.gd
@@ -3,9 +3,21 @@ extends TextureButton
 export var cost = 1
 export var unit_name = "Pawn"
 var teamsetup
+var name_label
 func _ready():
 	teamsetup = get_tree().root.get_node("TeamSetup")
 	$Cost.text = String(cost)
+	name_label = get_parent().get_parent().get_parent().get_node_or_null("Name")
+	connect("mouse_entered", self, "on_mouse_entered")
+	connect("mouse_exited", self, "on_mouse_exited")
+
+func on_mouse_entered():
+	self_modulate = Color.aqua
+	name_label.text = unit_name
+
+func on_mouse_exited():
+	self_modulate = Color.white
+	name_label.text = ""
 
 func _on_pressed():
 	if teamsetup.selected_cell:
