b0e4cde854019dcb5b1346575e3ffb900a279d4c
Paul Töpfer <hackerman@spaghetticoding.de>
2022-01-09 18:40:37 +0100

    Improved Turn counter and font readability

--- Scripts/TurnCounter.gd
+++ Scripts/TurnCounter.gd
@@ -5,8 +5,12 @@ var turn = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Helper.game_manager.connect("on_turn_begin", self, "new_turn")
+	Helper.game_manager.connect("on_turn_begin", self, "turn_begin")
+	Helper.game_manager.connect("on_turn_end", self, "turn_end")
 
-func new_turn():
+func turn_begin():
 	turn = turn+1
-	text = "Turn: " + String(turn)
+	text = "Turn: " + String(turn) + "\nPlayer phase"
+
+func turn_end():
+	text = "Turn: " + String(turn) + "\nEnemy phase"
